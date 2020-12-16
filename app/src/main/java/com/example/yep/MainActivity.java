95639202dfb077027cacf3d2757953b02b5e24e0
brandon <brandon.torres619@gmail.com>
2020-12-16 14:14:41 -0500

    buttonMaster

--- app/src/main/java/com/example/yep/MainActivity.java
+++ app/src/main/java/com/example/yep/MainActivity.java
@@ -9,6 +9,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
+    private Button button2;
+    private Button button3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,10 +25,38 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openActivityMain3();
+            }
+
+        });
+
+        button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openActivityMain4();
+            }
+
+        });
+
+
     }
 
     public void openActivityMain2() {
         Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }
+    public void openActivityMain3() {
+        Intent intent = new Intent(this, MainActivity3.class);
+        startActivity(intent);
+    }
+    public void openActivityMain4() {
+        Intent intent = new Intent(this, MainActivity4.class);
         startActivity(intent);
     }
 }
